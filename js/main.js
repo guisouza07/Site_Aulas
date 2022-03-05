57b6ce07492530eded4e11978791b5766f088f87
Guilherme Souza <gui_csouza@outlook.com>
2022-03-05 17:53:22 -0300

    update

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@
         autoplay: true,
         smartSpeed: 1000,
         center: true,
-        margin: 24,
+        margin: 10,
         dots: true,
         loop: true,
         nav : false,
@@ -100,7 +100,7 @@
                 items:2
             },
             992:{
-                items:3
+                items:2
             }
         }
     });
